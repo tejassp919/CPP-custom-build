fd5f6edcfaf4f60154dade1d679b197d53ded58e
Tejas Gajanan Patil <tpatil@ee.iitr.ac.in>
2020-03-19 21:16:36 +0530

    Add files via upload

---
+++ stdc++.h
@@ -0,0 +1,166 @@
+// C++ includes used for precompiling -*- C++ -*-
+
+// Copyright (C) 2003-2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// Under Section 7 of GPL version 3, you are granted additional
+// permissions described in the GCC Runtime Library Exception, version
+// 3.1, as published by the Free Software Foundation.
+
+// You should have received a copy of the GNU General Public License and
+// a copy of the GCC Runtime Library Exception along with this program;
+// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+/** @file stdc++.h
+ *  This is an implementation file for a precompiled header.
+ */
+
+#define tejasp
+
+// 17.4.1.2 Headers
+
+// C
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cuchar>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#endif
+
+using namespace std;
+
+template<typename T>
+void __p(T a) { cout << a << " "; }
+template<typename T, typename F>
+void __p(pair<T, F> a) { cout << "{ "; __p(a.first); __p(a.second); cout << "} "; }
+template<typename Arg1>
+void __f(const char *name, Arg1 &&arg1) {
+    cout<<name<<" : ";__p(arg1); cout<<endl;
+}
+template<typename Arg1>
+void __t(const char *name, Arg1 &&arg1)
+{ cout<<name<<" : { "; for (auto p : arg1) __p(p); cout<<"}"<<endl; }
+template<typename Arg1, typename ... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+    int bracket=0,i=0;
+    for(; ;i++)
+        if(names[i]==','&&bracket==0)
+            break;
+        else if(names[i]=='(')
+            bracket++;
+        else if(names[i]==')')
+            bracket--;
+    cout.write(names,i)<<" : ";
+    __p(arg1);  cout<<"| ";
+    __f(names+i+1,args...);
+}
+template<typename Arg1, typename Arg2>
+void __f(const char *names, Arg1 arg1[], Arg2 &&arg2){
+    int i=0;
+    for(; ;i++) if(names[i]==',') break;
+    cout.write(names,i)<<" : { ";
+    for(int i=0; i<arg2; arg2++) __p(arg1[i]);
+    cout << "} "<<endl;
+}
+#define trace(...) { cout<<"Line:"<<__LINE__<<" | "; __f(#__VA_ARGS__, __VA_ARGS__); }
+#define cotra(...) { cout<<"Line:"<<__LINE__<<" | "; __t(#__VA_ARGS__, __VA_ARGS__); }
+#define localsys 1
+int begtime = clock();
+#define end_routine() cout << "\n\nTime elapsed: " << (clock() - begtime)*1000/CLOCKS_PER_SEC << " ms\n\n";
