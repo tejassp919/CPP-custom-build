62f9eb09ede1e2fcf9e829505ce7d6b7f348af8e
Tejas Gajanan Patil <tpatil@ee.iitr.ac.in>
2020-06-07 18:25:15 +0530

    Update debugger.cpp

--- Library/debugger.cpp
+++ Library/debugger.cpp
@@ -1,14 +1,30 @@
 template<typename T>
 void __p(T a) { cout << a << " "; }
 template<typename T, typename F>
-void __p(pair<T, F> a) { cout << "{ "; __p(a.ff); __p(a.ss); cout << "} "; }
+void __p(pair<T, F> a) { cout << "{ "; __p(a.first); __p(a.second); cout << "} "; }
+template<typename T>
+void __p(vector<T> &a) { cout<<endl<<" { "; for (auto p : a) __p(p); cout<<"}"; }
+template<typename Arg1>
+void __t(const char *name, Arg1 &&arg1)
+{ cout<<name<<" : { "; for (auto p : arg1) __p(p); cout<<"}"<<endl; }
+template<typename Arg1, typename ... Args>
+void __t(const char *names, Arg1 &&arg1, Args &&... args) {
+    int bracket=0,i=0;
+    for(; ;i++)
+        if(names[i]==','&&bracket==0)
+            break;
+        else if(names[i]=='(')
+            bracket++;
+        else if(names[i]==')')
+            bracket--;
+    cout.write(names,i)<<" : ";
+    __p(arg1);  cout<<endl;
+    __t(names+i+1,args...);
+}
 template<typename Arg1>
 void __f(const char *name, Arg1 &&arg1) {
     cout<<name<<" : ";__p(arg1); cout<<endl;
 }
-template<typename Arg1>
-void __t(const char *name, Arg1 &&arg1)
-{ cout<<name<<" : { "; for (auto p : arg1) __p(p); cout<<"}"<<endl; }
 template<typename Arg1, typename ... Args>
 void __f(const char *names, Arg1 &&arg1, Args &&... args) {
     int bracket=0,i=0;
@@ -28,7 +44,7 @@ void __f(const char *names, Arg1 arg1[], Arg2 &&arg2){
     int i=0;
     for(; ;i++) if(names[i]==',') break;
     cout.write(names,i)<<" : { ";
-    fol(i,0,arg2) __p(arg1[i]);
+    for(int i=0; i<arg2; i++) __p(arg1[i]);
     cout << "} "<<endl;
 }
 #define trace(...) { cout<<"Line:"<<__LINE__<<" | "; __f(#__VA_ARGS__, __VA_ARGS__); }
